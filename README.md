237860e81026e095456799b284b91c3fca003b1b
Gustavo S. Teixeira <gsteixei@gmail.com>
2021-10-01 21:58:13 -0300

    refactor

--- README.md
+++ README.md
@@ -22,7 +22,12 @@ Create a network telling the size (nodes) of earch layer.
     // Make predictions
     predicted := nn.Predict(inputs[i])
 ```
-
+It is possible to use multiple hidden layers. Just pass the size of each one of them in the list:
+```go
+    // this will create a hidden layer of 3 layers, with sizes 4, 6 and 3.
+    nn := NewNeuralNetwork(size_input, size_output,
+                           []int {4, 6, 3})
+```
 ## Two implementations:
 
 There are two implementations on this repository:
--- neural.go
+++ neural.go
@@ -1,8 +1,10 @@
-// Go implementation of a simple Feedforward Neural Network
+// A simple feed forward XOR neural network in Go
 //
-// Author: Gustavo Selbach Teixeira (gsteixei@gmail.com)
+//  Allows for multiple hidden layers
+// 
+// Author: Gustavo Selbach Teixeira
 //
-//
+
 package main
 import (
     "fmt"
@@ -77,77 +79,8 @@ func (nn *NeuralNetwork) Set_learning_rate (rate float64) {
     nn.learning_rate = rate
 }
 
-// Make a prediction based. To be used once the network is trained
-func (nn *NeuralNetwork) Predict (inputs []float64) []float64{
-    nn.Set_inputs(inputs)
-    activation_function(nn.input_layer, nn.hidden_layer[0])
-    activation_function(nn.hidden_layer[0], nn.output_layer)
-    return nn.output_layer.values
-}
-
-// Train the neural network
-func (nn *NeuralNetwork) Train (inputs,
-                                outputs [][]float64,
-                                n_epochs int) {
-    var i int
-    var j int
-    num_training_sets := len(inputs)
-    // randomize training to avoid network to become grandmothered
-    training_sequence := make([]int, num_training_sets)
-    for i=0; i<num_training_sets; i++ {
-        training_sequence[i] = i
-    }
-    for n:=0; n<n_epochs; n++ {
-        shuffle_array(training_sequence)
-        for x:=0; x<num_training_sets; x++ {
-            i := training_sequence[x]
-            // Forward pass
-            nn.Set_inputs(inputs[i])
-            j = 0
-            activation_function(nn.input_layer, nn.hidden_layer[j])
-            for j < len(nn.hidden_layer)-1 {
-                activation_function(nn.hidden_layer[j],
-                                    nn.hidden_layer[j+1])
-                j++
-            }
-            activation_function(nn.hidden_layer[j], nn.output_layer)
-            // Show results
-            fmt.Println("Input: ", inputs[i],
-                        "Expected: ", outputs[i],
-                        "Output: ", nn.output_layer.values)
-            // Learning
-            // calculate the deltas
-            nn.calc_delta_output(outputs[i])
-            calc_deltas(nn.output_layer, nn.hidden_layer[0])
-            // update weights
-            update_weights(nn.output_layer, nn.hidden_layer[0],
-                           nn.learning_rate)
-            update_weights(nn.hidden_layer[0], nn.input_layer,
-                           nn.learning_rate)
-        }
-    }
-}
-
-// Calculate the delta for the output layer
-func (nn *NeuralNetwork) calc_delta_output(expected []float64) {
-    for i := range nn.output_layer.values {
-        errors := (expected[i] - nn.output_layer.values[i])
-        nn.output_layer.deltas[i] = (errors * d_sigmoid(nn.output_layer.values[i]))
-    }
-}
-
-// The logistical sigmoid function
-func sigmoid(x float64) float64 {
-    return 1 / (1 + math.Exp(-x))
-}
-
-// The derivative of sigmoid function
-func d_sigmoid(x float64) float64 {
-    return x * (1 - x)
-}
-
 // The activation function
-func activation_function(source, target Layer) {
+func (nn *NeuralNetwork) activation_function (source, target Layer) {
     var activation float64
     source_length := len(source.values)
     for j:=0; j<len(target.values); j++ {
@@ -160,7 +93,7 @@ func activation_function(source, target Layer) {
 }
 
 // Calculate the Deltas
-func calc_deltas(source, target Layer) {
+func (nn *NeuralNetwork) calc_deltas (source, target Layer) {
     var errors float64
     for j := range target.values {
         errors = 0.0
@@ -171,14 +104,102 @@ func calc_deltas(source, target Layer) {
     }
 }
 
+// Calculate the delta for the output layer
+func (nn *NeuralNetwork) calc_delta_output (expected []float64) {
+    for i := range nn.output_layer.values {
+        errors := (expected[i] - nn.output_layer.values[i])
+        nn.output_layer.deltas[i] = (errors * d_sigmoid(nn.output_layer.values[i]))
+    }
+}
+
 // Update the weights of the synapses
-func update_weights(source, target Layer, learning_rate float64) {
+func (nn *NeuralNetwork) update_weights (source, target Layer) {
     for j := range source.values {
-        source.bias[j] += (source.deltas[j] * learning_rate)
+        source.bias[j] += (source.deltas[j] * nn.learning_rate)
         for k := range target.values {
-            source.weights[k][j] += (target.values[k] * source.deltas[j] * learning_rate)
+            source.weights[k][j] += (target.values[k] * source.deltas[j] * nn.learning_rate)
         }
     }
+}
+
+// NN Activation step
+func (nn *NeuralNetwork) Forward_pass () {
+    var j int = 0
+    nn.activation_function(nn.input_layer, nn.hidden_layer[j])
+    for j < len(nn.hidden_layer)-1 {
+        nn.activation_function(nn.hidden_layer[j],
+                            nn.hidden_layer[j+1])
+        j++
+    }
+    nn.activation_function(nn.hidden_layer[j], nn.output_layer)
+}
+
+// Backpropagation learning process
+func (nn *NeuralNetwork) Back_propagation (outputs []float64) {
+    var last_hidden_layer, k int
+    // calculate the deltas
+    nn.calc_delta_output(outputs)
+    last_hidden_layer = len(nn.hidden_layer)-1
+    k = last_hidden_layer
+    // From output layer to the last of the hidden layers
+    nn.calc_deltas(nn.output_layer, nn.hidden_layer[k])
+    // Run though the hidden layers. If theres more than 1.
+    for k > 0 {
+        nn.calc_deltas(nn.hidden_layer[k], nn.hidden_layer[k-1])
+        k -= 1
+    }
+    // Update weights and bias
+    k = last_hidden_layer
+    nn.update_weights(nn.output_layer, nn.hidden_layer[k])
+    for k > 0 {
+        nn.update_weights(nn.hidden_layer[k], nn.hidden_layer[k-1])
+        k -= 1
+    }
+    // from output to hidden layer
+    nn.update_weights(nn.hidden_layer[k], nn.input_layer)
+}
+
+// Train the neural network
+func (nn *NeuralNetwork) Train (inputs, outputs [][]float64, n_epochs int) {
+    var i int
+    num_training_sets := len(inputs)
+    // randomize training to avoid network to become grandmothered
+    training_sequence := make([]int, num_training_sets)
+    for i=0; i<num_training_sets; i++ {
+        training_sequence[i] = i
+    }
+    for n:=0; n<n_epochs; n++ {
+        shuffle_array(training_sequence)
+        for x:=0; x<num_training_sets; x++ {
+            i := training_sequence[x]
+            // Forward pass
+            nn.Set_inputs(inputs[i])
+            nn.Forward_pass()
+            // Show results
+            fmt.Println("Input: ", inputs[i],
+                        "Expected: ", outputs[i],
+                        "Output: ", nn.output_layer.values)
+            // Learning
+            nn.Back_propagation(outputs[i])
+        }
+    }
+}
+
+// Make a prediction. To be used once the network is trained
+func (nn *NeuralNetwork) Predict (inputs []float64) []float64{
+    nn.Set_inputs(inputs)
+    nn.Forward_pass()
+    return nn.output_layer.values
+}
+
+// The logistical sigmoid function
+func sigmoid(x float64) float64 {
+    return 1 / (1 + math.Exp(-x))
+}
+
+// The derivative of sigmoid function
+func d_sigmoid(x float64) float64 {
+    return x * (1 - x)
 }
 
 // Shuffle array to a random order
@@ -188,8 +209,7 @@ func shuffle_array(arr []int) {
                      arr[i], arr[j] = arr[j], arr[i] })
 }
 
-
-// Set parameters and call training
+// Main function
 func main () {
     // set the parameters for training
     inputs := [][]float64 {{0.0, 0.0},
@@ -198,20 +218,17 @@ func main () {
                            {1.0, 1.0}}
     outputs := [][]float64 {{0.0}, {1.0}, {1.0}, {0.0}}
     // instantiate the network
-    num_hidden_nodes := 3
+    num_hidden_nodes := 4
     hidden_sizes := []int {num_hidden_nodes,}
-    nn := NewNeuralNetwork(len(inputs[0]),
-                           len(outputs[0]),
-                           hidden_sizes)
+    nn := NewNeuralNetwork(len(inputs[0]), len(outputs[0]), hidden_sizes)
     // start training
     iteractions := 10000
-    
     nn.Train(inputs, outputs, iteractions)
     // Test trained network
     for i := range inputs {
         predicted := nn.Predict(inputs[i])
-        fmt.Println("input: ", inputs[i],
-                    "predicted:", predicted,
-                    "output:", outputs[i])
+        fmt.Println("Input: ", inputs[i],
+                    "Expected: ", outputs[i],
+                    "Output: ", predicted)
     }
 }
